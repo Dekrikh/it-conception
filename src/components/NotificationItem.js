7bddcd5127e1e6d80d641520e6d0a8feb0d132a3
zergho_o <zergho_o@etna-alternance.net>
2023-09-12 13:09:46 +0200

    point vert

--- src/components/NotificationItem.js
+++ src/components/NotificationItem.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { CListGroupItem } from '@coreui/react';
+import { CListGroupItem, CBadge } from '@coreui/react';
+import './NotificationList.css';
 
 function NotificationItem({ notification, onClick }) {
-  return (
-    <CListGroupItem
-      onClick={onClick}
-      style={{ cursor: 'pointer' }}
-      className={notification.isAlreadyOpen ? 'notification-read' : ''}
-    >
-      {notification.title}
-    </CListGroupItem>
-  );
-}
+    return (
+      <CListGroupItem
+        onClick={onClick}
+        style={{ cursor: 'pointer' }}
+        className={notification.isAlreadyOpen ? 'notification-read' : ''}
+      >
+        {notification.title}
+        {!notification.isAlreadyOpen && (
+          <CBadge className="notification-badge">
+            &nbsp;
+          </CBadge>
+        )}
+      </CListGroupItem>
+    );
+  }
+  
 
 export default NotificationItem;
